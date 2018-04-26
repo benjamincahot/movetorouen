6bc26c1fc193e97ace8d90b929b023299d69a9b3
Florent Bodot <tiflobo76550@gmail.com>
2018-04-26 16:27:38 +0200

    updata

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new HomeBundle\HomeBundle(),
+            new Theme\CategoryBundle\CategoryBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/Theme/CategoryBundle/CategoryBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Theme\CategoryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CategoryBundle extends Bundle
+{
+}
