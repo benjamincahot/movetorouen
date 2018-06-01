5c013597d1f55fc9cf6813ad186fffe29c6b2021
Benjamin Cahot <benjamincahot@icloud.com>
2018-06-01 15:56:13 +0200

    jgkjhkjhkj

--- src/AdminBundle/Controller/AdminController.php
+++ src/AdminBundle/Controller/AdminController.php
@@ -10,4 +10,25 @@ class AdminController extends Controller
     {
         return $this->render('AdminBundle:Default:dashboard.html.twig');
     }
+
+    public function parksAction()
+    {
+        return $this->render('AdminBundle:Default:parks.html.twig');
+    }
+
+    public function eventsAction()
+    {
+        return $this->render('AdminBundle:Default:events.html.twig');
+    }
+
+    public function monumentsAction()
+    {
+        return $this->render('AdminBundle:Default:monuments.html.twig');
+    }
+
+    public function restaurantsAction()
+    {
+        return $this->render('AdminBundle:Default:restaurants.html.twig');
+    }
+
 }
