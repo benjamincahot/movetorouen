29057537b70aef738fab51c86979212fa62ab9ff
Florent Bodot <tiflobo76550@gmail.com>
2018-06-01 16:53:18 +0200

    up

--- src/Theme/MonumentBundle/Controller/DefaultController.php
+++ src/Theme/MonumentBundle/Controller/DefaultController.php
@@ -8,6 +8,26 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MonumentBundle:Default:index.html.twig');
+        return $this->render('@Monument/Default/index.html.twig');
+    }
+
+    public function addAction()
+    {
+        return $this->render('@Monument/Default/create.html.twig');
+    }
+
+    public function retrieveAction()
+    {
+        return $this->render('@Monument/Default/retrieve.html.twig');
+    }
+
+    public function updateAction()
+    {
+        return $this->render('@Monument/Default/update.html.twig');
+    }
+
+    public function deleteAction()
+    {
+        return $this->render('@Monument/Default/delete.html.twig');
     }
 }
--- src/Theme/ParcBundle/Controller/DefaultController.php
+++ src/Theme/ParcBundle/Controller/DefaultController.php
@@ -8,6 +8,26 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ParcBundle:Default:index.html.twig');
+        return $this->render('@Parc/Default/index.html.twig');
+    }
+
+    public function addAction()
+    {
+        return $this->render('@Parc/Default/create.html.twig');
+    }
+
+    public function retrieveAction()
+    {
+        return $this->render('@Parc/Default/retrieve.html.twig');
+    }
+
+    public function updateAction()
+    {
+        return $this->render('@Parc/Default/update.html.twig');
+    }
+
+    public function deleteAction()
+    {
+        return $this->render('@Parc/Default/delete.html.twig');
     }
 }
