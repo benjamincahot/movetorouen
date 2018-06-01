3d58e19bf1ed79d1c7f2193283a76ae9494e9436
Florent Bodot <tiflobo76550@gmail.com>
2018-06-01 12:37:51 +0200

    crud

--- src/Theme/EvenementBundle/Controller/DefaultController.php
+++ src/Theme/EvenementBundle/Controller/DefaultController.php
@@ -4,10 +4,34 @@ namespace Theme\EvenementBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Theme\EvenementBundle\Entity\Evenements;
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('@Evenement/Default/index.html.twig');
     }
+
+    public function addAction(Request $request)
+    {
+        return $this->render('@Evenement/Default/create.html.twig');
+    }
+
+    public function retrieveAction()
+    {
+        return $this->render('@Evenement/Default/retrieve.html.twig');
+    }
+
+    public function updateAction()
+    {
+        return $this->render('@Evenement/Default/update.html.twig');
+    }
+
+    public function deleteAction()
+    {
+        return $this->render('@Evenement/Default/delete.html.twig');
+    }
+
+
 }
