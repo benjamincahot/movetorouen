8957efaba9dc0959ab99129cd2ace3ac3eaf058c
Florent Bodot <tiflobo76550@gmail.com>
2018-06-04 14:55:21 +0200

    up

--- src/User/UserBundle/Entity/User.php
+++ src/User/UserBundle/Entity/User.php
@@ -6,26 +6,22 @@ namespace User\UserBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
-/*
+/**
  * @ORM\Entity
  * @ORM\Table(name="user")
  */
 class User extends BaseUser
 {
-    /*
+    /**
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
-    // protected $firstname;
-    // protected $lastname;
-
-
-    public function construct()
+    public function __construct()
     {
-        parent::construct();
+        parent::__construct();
         // your own logic
     }
 }
