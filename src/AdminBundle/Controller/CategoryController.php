8fff2b05b02b0d93017d3cfee20b053891a8ca16
Benjamin Cahot <benjamincahot@icloud.com>
2018-06-04 16:30:23 +0200

    formulaire trop complexe remix dubstep

--- src/AdminBundle/Controller/CategoryController.php
+++ src/AdminBundle/Controller/CategoryController.php
@@ -3,7 +3,10 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use HomeBundle\Entity\Category;
+use Symfony\Component\HttpFoundation\Request;
+use AdminBundle\Form\CategoryType;
+use \DateTime;
 class CategoryController extends Controller
 {
     // Categories
@@ -25,5 +28,55 @@ class CategoryController extends Controller
     public function restaurantsAction()
     {
         return $this->render('AdminBundle:Default:restaurants.html.twig');
+    }
+
+      // route method get
+    public function newAction()
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        // $form = $this->createFormBuilder($category)
+        //     ->add('name', TextType::class)
+        //     ->add('save', SubmitType::class, array('label' => 'Create Task'))
+        //     ->getForm();
+
+        return $this->render('AdminBundle:Category:new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+      // POST
+    public function newformAction(Request $request)
+    {
+      $category = new Category();
+      $form = $this->createForm(CategoryType::class, $category);
+
+      $form->handleRequest($request);
+
+          if ($form->isSubmitted() && $form->isValid()) {
+              $cat = $form->getData();
+
+              $cat->setCreatedAt(new DateTime());
+               $entityManager = $this->getDoctrine()->getManager();
+               $entityManager->persist($cat);
+               $entityManager->flush();
+
+              return $this->redirectToRoute('dashboard');
+          }
+
+          return $this->render('AdminBundle:Category:new.html.twig', array(
+              'form' => $form->createView(),
+          ));
+
+
+    }
+
+    public function listingAction()
+    {
+      // requete => all category
+      $entityManager = $this->getDoctrine()->getManager();
+
+      ///findAll();
+
     }
 }
--- src/AdminBundle/Controller/EventsAdminController.php
+++ src/AdminBundle/Controller/EventsAdminController.php
@@ -2,7 +2,12 @@
 
 namespace AdminBundle\Controller;
 
+use User\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EventsAdminController extends Controller
 {
