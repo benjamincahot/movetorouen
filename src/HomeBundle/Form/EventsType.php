772d64c8bf8d75ef512f9c21fecdf55e164088e7
Florent Bodot <tiflobo76550@gmail.com>
2018-06-05 11:40:14 +0200

    Flob

---
+++ src/HomeBundle/Form/EventsType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace HomeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('description')->add('price')->add('startDate')->add('endDate')->add('place')->add('category');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HomeBundle\Entity\Events'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'homebundle_events';
+    }
+
+
+}
