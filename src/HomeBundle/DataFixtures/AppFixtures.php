8cca12c8ae67561e0325b4f6273110b806b6c3db
Florent Bodot <tiflobo76550@gmail.com>
2018-06-11 15:00:30 +0200

    up

--- src/HomeBundle/DataFixtures/AppFixtures.php
+++ src/HomeBundle/DataFixtures/AppFixtures.php
@@ -56,6 +56,15 @@ class AppFixtures extends Fixture
       $this->addReference('szretujz', $user5);
       $manager->persist($user5);
 
+      $user6 = new User;
+      $user6->setUsername("User");
+      $user6->setEmail ('user@unknown.fr');
+      $user6->setPassword('$2y$13$Uscv2IOuYaAR1QQwrL.yzuDj8kiG484wReDOqb9out22k/FMya7.K');
+      $user6->setRoles(array('ROLE_USER'));
+      $user6->setEnabled(true);
+      $this->addReference('zeraztesze', $user6);
+      $manager->persist($user6);
+      
       $sport = new Category();
       $sport->setName("Sport");
       $manager->persist($sport);
