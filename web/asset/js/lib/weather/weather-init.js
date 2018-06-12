eba0a95c45550268ea3d9b26f0fcd555f5de0176
Benjamin Cahot <benjamincahot@icloud.com>
2018-06-12 14:08:29 +0200

    admin

--- web/asset/js/lib/weather/weather-init.js
+++ web/asset/js/lib/weather/weather-init.js
@@ -1,35 +1,28 @@
-(function ($) {
-    //    "use strict";
+(function($) {
 
-    function loadWeather(location, woeid) {
-        $.simpleWeather({
-            location: location,
-            woeid: woeid,
-            unit: 'c',
-            success: function (weather) {
+  function loadWeather(location, woeid) {
+    $.simpleWeather({
+      location: location,
+      woeid: woeid,
+      unit: 'c',
+      success: function(weather) {
 
-                html = '<i class="wi wi-yahoo-' + weather.code + '"></i><h2> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
-                html += '<div class="city">' + weather.city + ', ' + weather.region + '</div>';
-                html += '<div class="currently">' + weather.currently + '</div>';
-                html += '<div class="celcious">' + weather.alt.temp + '&deg;C</div>';
+        html = '<i class="wi wi-yahoo-' + weather.code + '"></i><h2> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+        html += '<div class="city">' + weather.city + ', ' + weather.region + '</div>';
+        html += '<div class="currently">' + weather.currently + '</div>';
+        html += '<div class="celcious">' + weather.alt.temp + '&deg;C</div>';
 
-                $("#weather-one").html(html);
-            },
-            error: function (error) {
-                $("#weather-one").html('<p>' + error + '</p>');
-            }
-        });
+        $("#weather-one").html(html);
+      },
+      error: function(error) {
+        $("#weather-one").html('<p>' + error + '</p>');
+      }
+    });
 
-        /* Where in the world are you? */
-        $('.js-geolocation').on('click', function() {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            loadWeather(position.coords.latitude + ',' + position.coords.longitude); //load weather using your lat/lng coordinates
-          });
-        });
-    }
+  }
 
 
-    // init
-    loadWeather('Rouen', '12597115');
+  // Si vous souhaitez modifier la ville qui s'affiche, entrer une ville + son WOEID (http://www.woeidlookup.com/)
+  loadWeather('Rouen', '12657347');
 
 })(jQuery);
