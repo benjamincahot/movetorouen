bf5ab0a761d198f4947cc1e22a697ca5ad922952
Florent.D <fdemech@gmail.com>
2018-06-14 15:12:46 +0200

    ddzazeazea

---
+++ src/HomeBundle/Form/ContactType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace HomeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+use HomeBundle\Entity\Contact;
+
+class ContactType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class)
+                ->add('email', TextType::class)
+                ->add('sujet', TextType::class)
+                ->add('message', TextareaType::class)
+                ->add('save', SubmitType::class, array('label' => 'Create Contact'))
+                        
+                ;
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HomeBundle\Entity\Contact',
+            //'mapped' => false
+        ));
+    }
+
+    
+
+
+}
